156377cab5e030804ff614bd425d53a3550b2204
marcihel05 <helena.marcius05@gmail.com>
2021-07-13 14:58:27 +0200

    maknula dvotocku

--- cinestar/view/employee/myInfo.php
+++ cinestar/view/employee/myInfo.php
@@ -1,48 +1,44 @@
 
 <?php include __DIR__ . '/../_header.php'; ?>
 
+
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="index.php?rt=ucenik">Home page</a></li>
-        <li class="breadcrumb-item active">My Info</li>
+        <li class="breadcrumb-item"><a href="index.php?rt=employee">Home page</a></li>
+        <li class="breadcrumb-item active">Your info</li>
     </ol>
 </nav>
-<h1 class="h2">Employee information</h1>
-<p>These are your information.</p>
+<h1 class="h2">Your info</h1>
+<p>Here you can change some of your information.</p>
 
 
-<!-- DEFINES ELEMENTS ON SAME HORIZONTAL LEVEL --->
-<div class="row">
-    <div class="col-12 col-xl-8 mb-4 mb-lg-0">
-        <div class="card">
-            <h5 class="card-header">Details</h5>
-            <div class="card-body">
-                                 
-                
-                <h5 class="card-title">Title</h5>
-                <p class="card-text">text </p>
-            
+<div class="col-12 col-xl-4 mb-4 mb-lg-0">
+<form action="index.php?rt=employee/otherSettingsCheck" method="post">
+			
+    <div class="form-group">
 
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon3">Email</span>
             </div>
-        </div> 
-    </div>
-
-
-    <div class="col-12 col-xl-4">
-        <div class="card">
-            <h5 class="card-header">Personal information :<h5>
-            <div class="card-body">
-              <h5 class="card-title">Username</h5>
-              <p ></p>
-              <h5 class="card-title">OIB</h5>
-              <p ></p>
-              <h5 class="card-title">Faculty name</h5>
-              <p ></p>
-
-            </div>
+            <input type="text" name="email" class="form-control" id="basic-url" placeholder="" aria-describedby="basic-addon3">
         </div>
-    </div>
-</div>
 
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon3">Password</span>
+            </div>
+            <input type="text" name="password"  class="form-control" id="basic-url" placeholder="******" aria-describedby="basic-addon3">
+        </div>
+        
+    
+        <button type="submit" class="btn btn-primary btn-block"> Change  </button>
+        
+        
+    </div> <!-- form-group// -->  
+					
+			                                                            
+</form>
+</div>
 
 <?php include __DIR__ . '/../_footer.php'; ?>
